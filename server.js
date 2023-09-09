bf3a60397223a0514838d1b276de06ced33a1b43
Jiyoh Macbook <geovannih514@gmail.com>
2023-09-09 16:17:27 -0700

    initial commit, installed node packages & create file structure

---
+++ server.js
@@ -0,0 +1,5 @@
+// install packages: express, mysql, sequelize, express-handlebars, nodemon, dotenv
+const express = require("express");
+const sequelize = require("./config/connection");
+
+const app = express();
